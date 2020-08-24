37c9ddd56e35c66f46c293f6409c80fffd8b4b8c
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-08-24 12:50:59 -0300

    Create Main File of version v0.8

---
+++ Samiki Tamaguchi v0.8/Samiki Tamaguchi v0.8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Samiki_Tamaguchi_v0._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Under Construction!");
+        }
+    }
+}
