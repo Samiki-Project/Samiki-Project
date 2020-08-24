cb343de6aaddd021544c13851d86065affde6a82
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-08-24 12:23:47 -0300

    Update file folder
    I put the Visual Studio project and all its subfolders in a single folder from version v0.5 (something you won't see because it is blocked by gitignore, but it is to organize the folders for myself)

---
+++ Samiki Tamaguchi v0.5/Samiki Tamaguchi v0.5/Main Character/MC.cs
@@ -0,0 +1,25 @@
+﻿namespace Samiki_Tamaguchi
+{
+    public partial class MC
+    {
+        /// <summary>
+        /// Main Character's Name
+        /// <para></para>
+        /// By default it is "Akira Mizushima", but can be customized.
+        /// </summary>
+        public string Name { get; set; } = "Akira Mizushima";
+
+        /// <summary>
+        /// Main Character's Money
+        /// <para></para>
+        /// By default it is 500 Yen, but it can be set or Randomized.
+        /// </summary>
+        public float Money { get; set; } = 500;
+
+        public MC(bool random = false)
+        {
+            if (random) GetRandomValues();
+        }
+
+    }
+}
