f7aeea117e10b217adde2928f4084d82460eb767
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-08-24 04:57:37 -0300

    Update Samiki Class (description)
    - Update Samiki Constructor
    - Create property value limitation [1]
    - Add support fields [2]
    
    [1]:
    Since I don't want the Happiness or Hunger value to be greater than 100 or less than 0, I have defined special occasions.
    If this property was been set with an incorrect value when instantiating the class, it'll correct the value
    
    [2]:
    I created these fields so that they can call its property (each field has its respective property) and define it avoiding the StackOverflow exception.

--- Samiki Tamaguchi v0.5/Samiki/Samiki.cs
+++ Samiki Tamaguchi v0.5/Samiki/Samiki.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Timers;
 using Samiki_Tamaguchi.Extensions;
 
 namespace Samiki_Tamaguchi
@@ -12,6 +11,19 @@ namespace Samiki_Tamaguchi
     {
 
         /// <summary>
+        /// Constructor for Samiki.
+        /// <para>Defines the default values for Samiki properties ​if not defined.</para>
+        /// Generates random numbers if the Boolean parameter is true
+        /// </summary>
+        /// <param name="random">Boolean value for generating random numbers, 
+        /// when true it generates random numbers for Samiki properties</param>
+        public Samiki(bool random = false)
+        {
+            if (random) GetRandomValues();
+            else { _Hunger = 100; _Hapiness = 100; }
+        }
+
+        /// <summary>
         /// Samiki's Name
         /// <para></para>
         /// By default it is "Samiki Kobayashi", but can be customized
@@ -21,12 +33,47 @@ namespace Samiki_Tamaguchi
         /// Samiki's Hapiness
         /// <para> By default it is 100, but can be random when creating Samiki</para>
         /// </summary>
-        public static int Hapiness { get; set; } = 100;
+        internal int Hapiness;
+        /// <summary>
+        /// Support field to call Property Happiness and define it avoiding StackOverflow Exception.
+        /// </summary>
+        public int _Hapiness{
+            get { return Hapiness; }
+            set
+            {
+                if (value < 0)
+                    Hapiness = 0;
+                else
+                if (value > 100)
+                    Hapiness = 100;
+                else
+                    Hapiness = value;
+            }
+        }
+
         /// <summary>
         /// Samiki's Hunger
         /// <para> By default it is 100, but can be random when creating Samiki</para>
         /// </summary>
-        public static int Hunger { get; set; } = 100;
+        internal int Hunger;
+
+        /// <summary>
+        /// Support field to call Property Hunger and define it avoiding StackOverflow Exception.
+        /// </summary>
+        public int _Hunger
+        {
+            get { return Hunger; }
+            set
+            {
+                if (value < 0)
+                    Hunger = 0;
+                else
+                if (value > 100)
+                    Hunger = 100;
+                else
+                    Hunger = value;
+            }
+        }
         /// <summary>
         /// Samiki's Health
         /// <para> By default it is 100, but can be random when creating Samiki</para>
@@ -39,18 +86,6 @@ namespace Samiki_Tamaguchi
         public bool Bipolar { get; set; } = false;
 
         /// <summary>
-        /// Constructor for Samiki.
-        /// <para>Defines the default values for Samiki properties ​if not defined.</para>
-        /// Generates random numbers if the Boolean parameter is true
-        /// </summary>
-        /// <param name="random">Boolean value for generating random numbers, 
-        /// when true it generates random numbers for Samiki properties</param>
-        public Samiki(bool random = false)
-        {
-            if (random) GetRandomValues();
-        }
-
-        /// <summary>
         /// Starts a Normal journey for Samiki
         /// <para>Creates a Interval for the functions:</para>
         /// [<see cref="GetAngry"/>] [<see cref="GetHungry"/>] and [<see cref="GetLifeless"/>]
@@ -60,8 +95,8 @@ namespace Samiki_Tamaguchi
             if (Bipolar) throw new Exception("Error: You cannot start a Normal journey if Samiki is bipolar");
             else
             {
-                Console.WriteLine("Hapiness = {0}\nHunger = {1}\nHealth = {2}", Hapiness, Hunger, Health);
-                MultiTimeOut.Start(5000, GetAngry, GetHungry, GetLifeless);
+                Console.WriteLine("\nHapiness = {0}\nHunger = {1}\nHealth = {2}", Hapiness, Hunger, Health);
+                MultiTimeOut.Start(1000, GetAngry, GetHungry, GetLifeless);
             }
         }
 
@@ -75,8 +110,8 @@ namespace Samiki_Tamaguchi
         {
             if (Bipolar)
             {
-                Console.WriteLine("Hapiness = {0}\nHunger = {1}\nHealth = {2}", Hapiness, Hunger, Health);
-                TimeOut.Start(BipolarWorld, 3000);
+                Console.WriteLine("\nHapiness = {0}\nHunger = {1}\nHealth = {2}", Hapiness, Hunger, Health);
+                TimeOut.Start(BipolarWorld, 500);
             }
             else throw new Exception("Error: You cannot start a Bipolar journey if Samiki is not Bipolar!");
         }
