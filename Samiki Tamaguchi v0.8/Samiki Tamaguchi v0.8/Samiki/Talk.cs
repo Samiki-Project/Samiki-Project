4c7e656c6acc513bec7413ff5b5e453739dbf3c1
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-08-27 11:45:01 -0300

    Create Talk method for Samiki Class

---
+++ Samiki Tamaguchi v0.8/Samiki Tamaguchi v0.8/Samiki/Talk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Samiki_Tamaguchi_v08
+{
+    public partial class Samiki
+    {
+        /// <summary>
+        /// Makes Samiki talk an sentence passed by parameter (String)
+        /// </summary>
+        /// 
+        /// <param name="sentence">The string which Samiki will speak</param>
+        public void Talk(string sentence) => Console.WriteLine($"Samiki: {sentence}");
+
+        public void Talk(params string[] sentence) => Console.WriteLine($"Samiki: {string.Join(' ', sentence)}");
+    }
+}
