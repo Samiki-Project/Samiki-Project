5a45ce90e24fe39bf9ff88b64696f270b3a4c371
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-08-27 11:44:29 -0300

    Create GettingOld Method for Samiki Class
    Method that must be executed inside a loop while the program is running!
    
    It generates an age for Samiki according to the time the Program was started and the current time (when run)

---
+++ Samiki Tamaguchi v0.8/Samiki Tamaguchi v0.8/Samiki/GettingOld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Samiki_Tamaguchi_v08
+{
+    public partial class Samiki
+    {
+        private DateTime BirthDate;
+
+        /// <summary>
+        /// When executed it creates a timestamp for generation of age property values,
+        /// Based on the <see cref="BirthDate"/> property that is
+        /// generated when creating the Samiki instance
+        /// </summary>
+        public void GettingOld()
+        {
+            TimeSpan Timestamp = DateTime.Now - BirthDate;
+            SamikiAge = Convert.ToInt32(Timestamp.TotalMinutes);
+            //Console.WriteLine($"{Age} minutos de idade");
+        }
+    }
+}
