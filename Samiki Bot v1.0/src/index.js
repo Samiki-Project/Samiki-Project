435049642af6cc7b448b90ea697da5830ae12a18
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-08-30 14:25:44 -0300

    Update Main Index to use Smart Command Handler

--- Samiki Bot v1.0/src/index.js
+++ Samiki Bot v1.0/src/index.js
@@ -1,32 +1,36 @@
 require('dotenv').config()
 require("module-alias/register")
 require('sk-database')
+const log = require('sk-log')("MAIN-BRAIN")
 
 const { Client } = require('discord.js')
-
 const client = new Client();
+
+client.commands = new Map()
+const { registerCommands, commandStatus } = require('./handlers/Command/registry');
 
 client.on('ready', () => console.log("I'm Ready! >.<"))
 
-client.on('message', async message =>{
-    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+client.on('message', async message => {
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-    if (!message.content.startsWith(process.env.PREFIX)) return;
+	if (message.author.bot) return;
+	if (message.channel.type === "dm") return;
+	if (!message.content.startsWith(process.env.PREFIX)) return;
 
-    try {
-		let commandFile = require(`./commands/${command}.js`)
-		commandFile.run(client, message, args);
+	if (client.commands.get(command)) client.commands.get(command)(client, message, args);
+	else message.channel.send("That command does not exist!")
+});
 
-	} catch (err) {
-		console.log(err)
-		if (err.code == 'MODULE_NOT_FOUND')
-			return message.channel.send("That command does not exist!")
-		console.log(err)
-	}
-
-})
-
-client.login(process.env.TOKEN)
+(async () => {
+	client.login(process.env.TOKEN);
+	await registerCommands(client, '../../commands')
+	setTimeout(() => {
+		for (command of commandStatus) {
+			if(command[1] === "Success") log(`The command '${command[0]}' was successfully loaded`, "success")
+			else if(command[1] === "Failed") log(`The command '${command[0]}' was NOT successfully loaded`, "error")
+			else log("There's Something wrong...", "warn")
+		}
+	}, 5000);
+})()
