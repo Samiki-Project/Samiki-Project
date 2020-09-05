572e9d1c1a80c4a13d181bb59b77e74ae398ce6c
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-09-05 10:08:21 -0300

    Create Commands
    Commands:
    addMoney (under Construction)
    removeMoney (under Construction)
    daily (under Construction)
    help (under Construction) [Test Phase]

---
+++ Samiki Bot v1.0/src/commands/economy/administration/removeMoney.js
@@ -0,0 +1,21 @@
+const { Client, Message } = require("discord.js")
+
+/**
+ * The scope of command execution
+ * @param {Client} client 
+ * @param {Message} message 
+ * @param {String} args 
+ */
+const run = async (client, message, args) => {
+
+}
+
+module.exports = {
+    help: {
+        name: "removeMoney",
+        description: "Command to Administration remove money to a member",
+        module: "Economy"
+    },
+    run: run,
+    aliases: []
+}
